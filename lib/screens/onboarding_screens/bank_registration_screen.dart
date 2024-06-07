00616b2d529a071df352fd69117e61fbcab69f7a
zack <paul.udah@flickssolutions.ng>
2024-06-07 17:40:03 +0100

    new file:   assets/svgs/bank.svg 	new file:   assets/svgs/car.svg 	new file:   assets/svgs/checker.svg 	new file:   assets/svgs/doc.svg 	new file:   assets/svgs/idcard.svg 	new file:   assets/svgs/license.svg 	new file:   assets/svgs/smallcar.svg 	new file:   assets/svgs/trafficcar.svg 	deleted:    lib/screens/active_trip_detail_view.dart 	renamed:    lib/screens/change_password_screen.dart -> lib/screens/authentication_screens/change_password_screen.dart 	renamed:    lib/screens/forget_password.dart -> lib/screens/authentication_screens/forget_password.dart 	renamed:    lib/screens/login_screen.dart -> lib/screens/authentication_screens/login_screen.dart 	renamed:    lib/screens/mail_sent_screen.dart -> lib/screens/authentication_screens/mail_sent_screen.dart 	renamed:    lib/screens/otp_screen.dart -> lib/screens/authentication_screens/otp_screen.dart 	renamed:    lib/screens/reset_password.dart -> lib/screens/authentication_screens/reset_password.dart 	renamed:    lib/screens/security_screen.dart -> lib/screens/authentication_screens/security_screen.dart 	renamed:    lib/screens/signup_screen.dart -> lib/screens/authentication_screens/signup_screen.dart 	new file:   lib/screens/onboarding_screens/bank_registration_screen.dart 	new file:   lib/screens/onboarding_screens/complete_profiles_screen.dart 	new file:   lib/screens/onboarding_screens/id_registration_screen.dart 	new file:   lib/screens/onboarding_screens/license_registration_screen.dart 	new file:   lib/screens/onboarding_screens/vehicle_registration_screen.dart 	renamed:    lib/screens/profile_edit_screen.dart -> lib/screens/profile_screens/profile_edit_screen.dart 	renamed:    lib/screens/profile_screen.dart -> lib/screens/profile_screens/profile_screen.dart 	renamed:    lib/screens/vehicle_details_screen.dart -> lib/screens/profile_screens/vehicle_details_screen.dart 	deleted:    lib/screens/requests_view.dart 	renamed:    lib/screens/chat_screen.dart -> lib/screens/trip_screens/chat_screen.dart 	renamed:    lib/screens/history_screen.dart -> lib/screens/trip_screens/history_screen.dart 	renamed:    lib/screens/ride_histories_detials_screen.dart -> lib/screens/trip_screens/ride_histories_detials_screen.dart 	renamed:    lib/screens/trip_completed.dart -> lib/screens/trip_screens/trip_completed.dart 	deleted:    lib/screens/trips_view.dart 	new file:   lib/widgets/dash_boarder_painter.dart

---
+++ lib/screens/onboarding_screens/bank_registration_screen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:ride_on_driver/core/constants/assets.dart';
+import 'package:ride_on_driver/core/extensions/build_context_extensions.dart';
+import 'package:ride_on_driver/widgets/app_elevated_button.dart';
+import 'package:ride_on_driver/widgets/app_text_field.dart';
+import 'package:ride_on_driver/widgets/spacing.dart';
+
+import '../../core/constants/colors.dart';
+
+class BankDetailsScreen extends StatefulWidget {
+  const BankDetailsScreen({super.key});
+
+  @override
+  State<BankDetailsScreen> createState() => _BankDetailsScreenState();
+}
+
+class _BankDetailsScreenState extends State<BankDetailsScreen> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  final TextEditingController accountNameController = TextEditingController();
+  final TextEditingController accountNumberController = TextEditingController();
+  final TextEditingController bankNameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Bank Account Details',
+            style: context.textTheme.bodyMedium!.copyWith(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                fontFamily: 'SFPRODISPLAYREGULAR')),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 15,
+            )),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 15, right: 15),
+
+        ///background image
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.assetsImagesPatternBackground),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(
+              color: AppColors.grey.withOpacity(0.7),
+            ),
+
+            const VerticalSpacing(10),
+
+            ///account name
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Account Name',
+                  style: context.textTheme.bodyMedium!.copyWith(
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
+                ),
+                const VerticalSpacing(10),
+                AppTextField(
+                  controller: accountNameController,
+                  hintText: 'Account name here',
+                ),
+              ],
+            ),
+
+            const VerticalSpacing(10),
+
+            ///account number
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Account Number',
+                  style: context.textTheme.bodyMedium!.copyWith(
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
+                ),
+                const VerticalSpacing(10),
+                AppTextField(
+                  keyboardType: TextInputType.number,
+                  controller: accountNumberController,
+                  hintText: 'xxxxxxxxxxx',
+                ),
+              ],
+            ),
+
+            const VerticalSpacing(10),
+
+            ///bank name
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Bank Name',
+                  style: context.textTheme.bodyMedium!.copyWith(
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
+                ),
+                const VerticalSpacing(10),
+                AppTextField(
+                  controller: bankNameController,
+                  hintText: 'Bank name here',
+                ),
+              ],
+            ),
+
+            const Spacer(),
+
+            ///save button
+            AppElevatedButton.large(
+              onPressed: context.pop,
+              text: 'Save',
+              backgroundColor: AppColors.black,
+              foregroundColor: AppColors.yellow,
+            ),
+            const VerticalSpacing(30),
+          ],
+        )),
+      ),
+    );
+  }
+}
